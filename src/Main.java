c3ad8bf4332594f641f99099483a49dacd4d2045
Juan David <juan.ramirez.freire@udla.edu.ec>
2025-06-24 11:02:08 -0500

    feat: añadir Despedida personalizada

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Saludo saludo = new Saludo();
-        System.out.println(saludo.generarSaludo("Mundo"));
+        // Saludo saludo = new Saludo();
+        // System.out.println(saludo.generarSaludo("Mundo"));
+        Despedida despedida = new Despedida();
+        System.out.println(despedida.generarDespedida("Mundo"));
     }
 }
