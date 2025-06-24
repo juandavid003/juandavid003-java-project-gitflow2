a55d1e4c9de0541d2696e968653ecf23f8404ad3
Juan David <juan.ramirez.freire@udla.edu.ec>
2025-06-24 11:10:02 -0500

    Start

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Saludo saludo = new Saludo();
-        System.out.println("***************");
-        System.out.println("* JAVA PROJECT *");
-        System.out.println("***************");
         System.out.println(saludo.generarSaludo("Mundo"));
     }
 }
